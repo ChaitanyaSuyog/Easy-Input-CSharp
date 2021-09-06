352a5dfdc82ac455fd50d7f583985a7bf3baece2
Chaitanya S <chaitanya.suyog@gmail.com>
2021-09-06 11:24:33 +0530

    Example.cs
    A test class- used for testing the two input systems.

---
+++ EasyInput/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EasyInput
+{
+    public class Execute
+    {
+        static void Main()
+        {
+            var input = EasyInput.InputLine("How's the weather today? ");
+            Console.WriteLine(input);
+        }
+    }
+}
