22c1267d86471787ed55d2d618dcf66258710dd3
Chaitanya S <chaitanya.suyog@gmail.com>
2021-09-06 09:06:35 +0530

    Created EasyInput.cs Class
    Created the above mentioned class to try and simplify the input system even further- making it almost Python-like.

---
+++ EasyInput/EasyInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EasyInput
+{
+    class EasyInput
+    {
+        public static void ExecuteLine(string str)
+        {
+            Console.WriteLine(str);
+            var inputGiven = Console.ReadLine();
+
+            var inputGivenString = typeof(string);
+            var inputGivenChar = typeof(char);
+            var inputGivenSByte = typeof(sbyte);
+            var inputGivenByte = typeof(byte);
+            var inputGivenInt = typeof(int);
+            var inputGivenUInt = typeof(uint);
+            var inputGivenShort = typeof(short);
+            var inputGivenLong = typeof(long);
+            var inputGivenULong = typeof(ulong);
+            var inputGivenDouble = typeof(double);
+            var inputGivenFloat = typeof(float);
+            var inputGivenDecimal = typeof(decimal);
+            var inputGivenBoolean = typeof(bool);
+        }
+    }
+}
